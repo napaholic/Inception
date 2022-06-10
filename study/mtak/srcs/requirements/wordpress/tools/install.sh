0a1e9a30c8f9120423c72310917c528a7f2e0714
napaholic <jaewkim@student.42seoul.kr>
2022-06-11 02:08:55 +0900

    study mtak

--- study/mtak/srcs/requirements/wordpress/tools/install.sh
+++ study/mtak/srcs/requirements/wordpress/tools/install.sh
@@ -3,7 +3,7 @@ apt-get update
 apt-get upgrade -yq
 apt-get install -yq php7.3-fpm php7.3-mysql mariadb-client wget
 wget https://raw.githubusercontent.com/wp-cli/builds/gh-pages/phar/wp-cli.phar
-chmod +x wp-cli.phar
+chmod +x wp-cli.phar #wp-cli.phar파일 실행할 권한부여
 mv wp-cli.phar /usr/local/bin/wp
 mv ./www.conf /etc/php/7.3/fpm/pool.d/www.conf
 mkdir -p /var/www/wordpress
--- study/mtak/srcs/requirements/wordpress/tools/start.sh
+++ study/mtak/srcs/requirements/wordpress/tools/start.sh
@@ -2,10 +2,10 @@
 sleep 5
 FILE=/var/www/wordpress/wp-config.php 
 if [ ! -f "$FILE" ]; then
-	wp cli update
-   	wp core download --allow-root --path='/var/www/wordpress'
+	wp cli update #워드프레스 명령창에서 관리하는 유틸리티
+   	wp core download --allow-root --path='/var/www/wordpress'#워드프레스 최종버전 디렉토리로 다운로드
 	wp core config \
-		--allow-root \
+		--allow-root \ #관리자권한옵션
 		--dbname=$DB_NAME \
 		--dbuser=$DB_LOGIN \
 		--dbpass=$DB_PASS \
@@ -18,14 +18,14 @@ if [ ! -f "$FILE" ]; then
 		--title=$WP_TITLE \
 		--admin_user=$WP_ADMIN \
 		--admin_password=$WP_ADMINPASS \
-		--admin_email="mtak0235@gmail.com" \
+		--admin_email="jaewkim@student.42seoul.kr" \
 		--skip-email \
 		--path='/var/www/wordpress'
 
 	wp user create \
 		--allow-root \
 		$WP_USER \
-		irrelevant@email.com \
+		kimja5163@gmail.com \
 		--user_pass=$WP_USERPASS \
 		--role=author
 fi
