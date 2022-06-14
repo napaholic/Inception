fc633d976741b9eb364a243d73c2fd0f631c947a
napaholic <jaewkim@student.42seoul.kr>
2022-06-14 16:22:43 +0900

    run.sh change

--- study/mtak/srcs/requirements/mariadb/tools/mariadb.sh
+++ study/mtak/srcs/requirements/mariadb/tools/mariadb.sh
@@ -1,10 +1,10 @@
 if [ ! -d /var/lib/mysql/$DB_NAME ]; then	#Test whether wordpress is already installed
     service mysql start
-    mysql "CREATE DATABASE IF NOT EXISTS $DB_NAME;"
-    mysql "CREATE USER '$DB_LOGIN'@'%' identified by '$DB_PASS';"
-    mysql "GRANT ALL PRIVILEGES ON $DB_NAME.* TO '$DB_LOGIN'@'%';"
-    mysql "ALTER USER 'root'@'localhost' IDENTIFIED BY '$DB_ROOTPASS';"
-    mysql "FLUSH PRIVILEGES;"
+    mysql -e "CREATE DATABASE IF NOT EXISTS $DB_NAME;"
+    mysql -e "CREATE USER '$DB_LOGIN'@'%' identified by '$DB_PASS';"
+    mysql -e "GRANT ALL PRIVILEGES ON $DB_NAME.* TO '$DB_LOGIN'@'%';"
+    mysql -e "ALTER USER 'root'@'localhost' IDENTIFIED BY '$DB_ROOTPASS';"
+    mysql -e "FLUSH PRIVILEGES;"
     mysqladmin -u root -p$DB_ROOTPASS shutdown
 fi
 
