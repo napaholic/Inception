7eaa2f046638b401b1d29653eec536a2edf9b50f
Jaewook Kim <jaewkim@c3r5s7.42seoul.kr>
2022-06-22 16:11:29 +0900

    asdf

--- srcs/requirements/mariadb/conf/run.sh
+++ srcs/requirements/mariadb/conf/run.sh
@@ -3,10 +3,10 @@ service mysql start;
 
 mysql -e "CREATE DATABASE IF NOT EXISTS $DB_NAME;"
 mysql -e "CREATE USER '$DB_LOGIN'@'%' IDENTIFIED BY '$DB_PASS';"
-mysql -e "GRANT ALL PRIVILEGES ON $DB_NAME.* TO '$DB_LOGIN'@'%';"
-mysql -e "ALTER USER 'root'@'localhost' IDENTIFIED BY '$DB_ROOTPASS';"
-mysql -e "GRANT ALL PRIVILEGES ON *.* TO 'user'@'%' IDENTIFIED by '';"
+mysql -e "GRANT ALL PRIVILEGES ON *.* TO '$DB_LOGIN'@'%';"
 mysql -e "FLUSH PRIVILEGES;"
+mysql -e "ALTER USER 'root'@'localhost' IDENTIFIED BY '$DB_ROOTPASS';"
+
 
 mysqladmin -u root -p$DB_ROOTPASS shutdown
 
