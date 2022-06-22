02c9078aaa8ecfe57e4722446f1d9e10c3783dc2
Jaewook Kim <jaewkim@c3r5s7.42seoul.kr>
2022-06-22 15:36:08 +0900

    asdf

--- srcs/requirements/mariadb/conf/run.sh
+++ srcs/requirements/mariadb/conf/run.sh
@@ -1,12 +1,12 @@
 #!/bin/sh
 service mysql start;
 
-mysql -e "CREATE DATABASE IF NOT EXISTS $WP_DB_NAME;"
-mysql -e "CREATE USER '$DB_ADMIN_NAME'@'%' IDENTIFIED BY '$DB_ADMIN_PASSWORD';"
-mysql -e "GRANT ALL PRIVILEGES ON $WP_DB_NAME.* TO '$DB_ADMIN_NAME'@'%';"
-mysql -e "ALTER USER 'root'@'localhost' IDENTIFIED BY '$MYSQL_ROOT_PW';"
+mysql -e "CREATE DATABASE IF NOT EXISTS $DB_NAME;"
+mysql -e "CREATE USER '$DB_LOGIN'@'%' IDENTIFIED BY '$DB_PASS';"
+mysql -e "GRANT ALL PRIVILEGES ON $DB_NAME.* TO '$DB_LOGIN'@'%';"
+mysql -e "ALTER USER 'root'@'localhost' IDENTIFIED BY '$DB_ROOTPASS';"
 mysql -e "FLUSH PRIVILEGES;"
 
-mysqladmin -u root -p$MYSQL_ROOT_PW shutdown
+mysqladmin -u root -p$DB_ROOTPASS shutdown
 
 exec mysqld_safe
