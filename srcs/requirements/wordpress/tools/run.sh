bcf7ed7d4a1ccb5c1e8ea80c94bbb2a22807b7ff
Jaewook Kim <jaewkim@c3r5s7.42seoul.kr>
2022-06-24 12:51:16 +0900

    nginx in fix

---
+++ srcs/requirements/wordpress/tools/run.sh
@@ -0,0 +1,32 @@
+sleep 5
+if [ ! -e /var/www/wordpress/wp-config.php ]; then
+# wp-config.php file 만들기
+    until   wp config create    --allow-root
+    							--dbname=$DB_NAME\
+    							--dbuser=$DB_LOGIN\
+                                --dbpass=$DB_PASS
+                                --dbhost=mariadb:3306\
+                                --path='/var/www/wordpress'; do
+            echo "Waiting for mariadb to start..."
+		    sleep 2
+    done
+    # wordpress 세팅하기
+    # --allow-root : 관리자 권한 허용
+    wp core install     --allow-root		\
+    					--url=$DOMAIN	\
+    					--title=$WP_TITLE	\
+    					--admin_user=$WP_ADMIN	\
+                        --admin_password=$WP_ADMINPASS	\
+                        --admin_email="jaewkim@student.42seoul.com"	\
+                        --path='/var/www/wordpress' >> /log.txt
+    wp user create      --allow-root $WP_USER $WP_EMAIL2	\
+    					--user_pass=$WP_USERPASS --role=author	\
+                        --path='/var/www/wordpress' >> /log.txt
+fi
+
+# foreground로 실행하기
+mkdir ./run/php/
+/usr/sbin/php-fpm7.3 -F
+
+# user 확인하기
+# wp user list --allow-root
