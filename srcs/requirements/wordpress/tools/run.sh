9c0e5855e1ac6f0c81de96562ba3aa3f99e48ce1
Jaewook Kim <jaewkim@c3r5s7.42seoul.kr>
2022-06-27 21:18:32 +0900

    roll back in wordpress

--- srcs/requirements/wordpress/tools/run.sh
+++ srcs/requirements/wordpress/tools/run.sh
@@ -8,10 +8,16 @@ if [ ! -e /var/www/wordpress/wp-config.php ]; then
     done
     # wordpress 세팅하기
     # --allow-root : 관리자 권한 허용
-    wp core install     --allow-root --url=$DOMAIN_NAME --title=$WP_TITLE --admin_user=$WP_USER1_NAME\
-                        --admin_password=$WP_USER1_PASSWORD --admin_email=$WP_EMAIL1 --path='/var/www/wordpress'
-    wp user create      --allow-root $WP_USER2_NAME $WP_EMAIL2 --user_pass=$WP_USER2_PASSWORD --role=author\
-                        --path='/var/www/wordpress' >> /log.txt
+    wp core install     --allow-root --url=$DOMAIN_NAME \
+    					--title=$WP_TITLE \
+    					--admin_user=$WP_USER1_NAME \
+                        --admin_password=$WP_USER1_PASSWORD \
+                        --admin_email=$WP_EMAIL1 \
+                        --path='/var/www/wordpress'
+    wp user create      --allow-root $WP_USER2_NAME $WP_EMAIL2 \
+    					--user_pass=$WP_USER2_PASSWORD \
+    					--role=author\
+                        --path='/var/www/wordpress'
 fi
 
 # foreground로 실행하기
