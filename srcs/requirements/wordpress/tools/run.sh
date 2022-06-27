a6e71eb2c01c3e4d6678d5c419dc347a4406862d
Jaewook Kim <jaewkim@c3r5s7.42seoul.kr>
2022-06-27 20:11:42 +0900

    Asdf

--- srcs/requirements/wordpress/tools/run.sh
+++ srcs/requirements/wordpress/tools/run.sh
@@ -1,30 +1,32 @@
 #!bin/bash
 sleep 5
-if [ ! -f /var/www/wordpress/wp-config.php ]; then
-# wp-config.php file 만들기
+FILE=/var/www/wordpress/wp-config.php
+if [ ! -f "$FILE" ]; then
 	wp cli update
-	wp core download --allow-root --path='/var/www/wordpress'
-    wp core config		--allow-root\
-    					--dbname=$DB_NAME\
-    					--dbuser=$DB_LOGIN\
-    					--dbpass=$DB_PASS\
-    					--dbhost=mariadb:3306\
-    					--path='/var/www/wordpress'
-	sleep 2
-    # wordpress 세팅하기
-    # --allow-root : 관리자 권한 허용
-    wp core install     --allow-root		\
-    					--url=$DOMAIN	\
-    					--title=$WP_TITLE	\
-    					--admin_user=$WP_ADMIN	\
-                        --admin_password=$WP_ADMINPASS	\
-                        --admin_email="jaewkim@student.42seoul.com"
-    wp user create      --allow-root	$WP_USER \
-    									$WP_EMAIL2	\
-    					--user_pass=$WP_USERPASS \
-    					--role=author
-fi
+   	wp core download --allow-root --path='/var/www/wordpress'
+	wp core config \
+		--allow-root \
+		--dbname=$DB_NAME \
+		--dbuser=$DB_LOGIN \
+		--dbpass=$DB_PASS \
+		--dbhost=mariadb:3306 \
+		--path='/var/www/wordpress'
 
-# foreground로 실행하기
-touch /run/php/php7.3-fpm.pid
+	wp core install \
+		--allow-root \
+		--url=$DOMAIN \
+		--title=$WP_TITLE \
+		--admin_user=$WP_ADMIN \
+		--admin_password=$WP_ADMINPASS \
+		--admin_email="jaewkim@gmail.com" \
+		--skip-email \
+		--path='/var/www/wordpress'
+
+	wp user create \
+		--allow-root \
+		$WP_USER \
+		jay@email.com \
+		--user_pass=$WP_USERPASS \
+		--role=author
+fi
 php-fpm7.3 --nodaemonize
