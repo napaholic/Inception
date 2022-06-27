509371e077117a11b15a997de56e4fc07aa76788
Jaewook Kim <jaewkim@c3r5s7.42seoul.kr>
2022-06-27 22:03:37 +0900

    install fix

--- srcs/requirements/wordpress/tools/run.sh
+++ srcs/requirements/wordpress/tools/run.sh
@@ -1,21 +1,21 @@
-
+#!/bin/bash
 if [ ! -e /var/www/wordpress/wp-config.php ]; then
 # wp-config.php file 만들기
-    until   wp config create    --allow-root --dbname=$WP_DB_NAME --dbuser=$DB_ADMIN_NAME\
-                                --dbpass=$DB_ADMIN_PASSWORD --dbhost=mariadb:3306 --path='/var/www/wordpress'; do
-            echo "Waiting for mariadb to start..."
-		    sleep 2
+    until   wp config create	--allow-root \
+    							--dbname=$WP_DB_NAME \
+    							--dbuser=$DB_ADMIN_NAME\
+                                --dbpass=$DB_ADMIN_PASSWORD \
+                                --dbhost=mariadb:3306 --path='/var/www/wordpress'
     done
-    # wordpress 세팅하기
     # --allow-root : 관리자 권한 허용
-    wp core install     --allow-root --url=$DOMAIN_NAME \
+    wp core install     --allow-root --url=$DOMAIN \
     					--title=$WP_TITLE \
-    					--admin_user=$WP_USER1_NAME \
-                        --admin_password=$WP_USER1_PASSWORD \
+    					--admin_user=$WP_ADMIN \
+                        --admin_password=$WP_ADMINPASS \
                         --admin_email=$WP_EMAIL1 \
                         --path='/var/www/wordpress'
-    wp user create      --allow-root $WP_USER2_NAME $WP_EMAIL2 \
-    					--user_pass=$WP_USER2_PASSWORD \
+    wp user create      --allow-root $WP_USER $WP_EMAIL2 \
+    					--user_pass=$WP_USERPASS \
     					--role=author\
                         --path='/var/www/wordpress'
 fi
