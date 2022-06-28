c92c6d06be203f76200917bb95e07a57db854e32
Jaewook Kim <jaewkim@c3r5s7.42seoul.kr>
2022-06-28 19:18:37 +0900

    first judge

--- study/proto/srcs/requirements/nginx/tools/install.sh
+++
@@ -1,6 +0,0 @@
-#!bin/bash
-apt-get update
-apt-get install -yq nginx openssl
-rm -rf /var/www/html/*
-openssl req -newkey rsa:4096 -days 365 -nodes -x509 -subj "/C=KR/ST=Seoul/L=Seoul/O=42Seoul/OU=Tak/CN=localhost" -keyout /etc/ssl/private/jaewkim.key -out /etc/ssl/certs/jaewkim.crt
-chmod 600 /etc/ssl/certs/jaewkim.crt /etc/ssl/private/jaewkim.key
